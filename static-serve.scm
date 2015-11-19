048dfe10d0f9d5c9468f8baf09450d0f28bda755
Kooda <kooda@upyum.com>
2015-11-19 14:04:33 +0100

    Add access logging to stderr

--- static-serve.scm
+++ static-serve.scm
@@ -6,6 +6,8 @@
 
   (parameterize ((root-path ".")
                  (handle-directory simple-directory-handler)
-                 (server-bind-address "0.0.0.0"))
+                 (server-bind-address "0.0.0.0")
+                 (access-log (current-error-port))
+                 (error-log (current-error-port)))
     (printf "Server listening on ~A:~A~N" (server-bind-address) (server-port))
     (start-server)))
